f69fbd5bead9c8a55b8cfa86bd8708be9bbec66a
Shani Patel <shanipatel37780@gmail.com>
2026-07-19 11:40:56 +0530

    add of some array

---
+++ add.js
@@ -0,0 +1,14 @@
+// Array add and remove element
+
+let object = {
+    name: "ram",
+    age: "20"
+}
+let arr = ["apple", 'banana', 'ram', object]
+// arr.push('orange')
+arr.pop()
+arr.pop()
+// console.log(arr)
+arr.unshift("orange")
+arr.shift()
+console.log(arr)
